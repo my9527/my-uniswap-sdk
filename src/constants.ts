2d94da42bac73c8949b7c18ca7ed3d7fd70df3b3
YangYang <yangyang@YangYangdeMacBook-Pro.local>
2023-03-21 23:22:49 +0800

    mod

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,9 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  FILE = 314,
+  FILEH = 3141
 }
 
 export enum TradeType {
@@ -22,9 +24,10 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x022831C01390280BD03Ed2681cD96b49B9018c4E'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+// export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xac8a14026b422ca0da671920456fa979148af3c16ade2989d491008c070635e4'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
